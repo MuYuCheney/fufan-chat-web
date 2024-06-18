c4c8e0cb35c25d4732876acbb7cb303be8e018e9
liufuyuan <18210049914@163.com>
2024-06-19 05:00:01 +0800

    feat: chat接口报错处理

--- src/api/chat/index.ts
+++ src/api/chat/index.ts
@@ -1,4 +1,5 @@
 // import { request } from "@/utils/service"
+import { ElMessage } from "element-plus"
 import type * as Chat from "./types/chat"
 import { fetchEventSource } from "@microsoft/fetch-event-source"
 
@@ -13,6 +14,7 @@ import { fetchEventSource } from "@microsoft/fetch-event-source"
 
 /** 聊天对话接口 */
 export function chatApi(data: Chat.ChatRequestData, options?: Chat.ChatFetchEventOptions) {
+  const ctrl = new AbortController()
   const bodyStr = JSON.stringify(data)
   // http://192.168.110.131:8000
   fetchEventSource("/api/chat", {
@@ -21,6 +23,13 @@ export function chatApi(data: Chat.ChatRequestData, options?: Chat.ChatFetchEven
       "Content-Type": "application/json"
     },
     body: bodyStr,
-    ...options
+    ...options,
+    signal: ctrl.signal,
+    onerror: async (err: any) => {
+      options?.onerror?.(err)
+      ElMessage.error("对话请求发生网络错误或涉及违规话题")
+      ctrl.abort()
+      throw err // 直接抛出错误，避免反复调用
+    }
   })
 }
--- src/api/chat/types/chat.ts
+++ src/api/chat/types/chat.ts
@@ -4,6 +4,7 @@ export interface IMessageData {
 
 export interface ChatFetchEventOptions {
   onmessage(data: IMessageData): Promise<void>
+  onerror?(error: string): Promise<void>
 }
 
 export interface ChatRequestData {
